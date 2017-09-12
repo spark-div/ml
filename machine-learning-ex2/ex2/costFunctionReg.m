db9b908b37ebe22bdedd6bae36faecd4cf2ec016
Aleksandr Sarapin <a.sarapin@surc.local>
2017-09-12 15:42:37 +0300

    ex2 CostFunc and Grad implemented

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -6,7 +6,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
+szt = length(theta);
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
@@ -17,10 +17,23 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+   Hth = sigmoid(X*theta);
+   term1 = ((-y)')*(log(Hth));
+   term2 = ((1.-y)')*log(1.-Hth);
+   SigMega= term1-term2;
+   J1st = sum(SigMega)/m;
+   regthet = theta(2:szt, 1);
+   SumSqrd = sum(regthet.^2);
+   Jreg = (lambda*SumSqrd)/(2*m);
+   J = J1st + Jreg;
 
-
-
-
+   C = Hth - y;
+   D = (C'*X)';
+   L = lambda*ones(szt, 1);
+   L(1) = 0;
+   RegTheta = L/m;
+   RegTheta = RegTheta .* theta;
+   grad = D/m + RegTheta; 
 
 % =============================================================
 
